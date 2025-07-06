b3da9fb8cc421802171592258df6321396e368dc
Mohammmed.Naseef <mohammed.naseef@ril.com>
2025-07-06 12:46:10 +0530

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React,{lazy,Suspense} from "react"
 import ReactDOM from "react-dom/client"
 import Header from "./components/Header"
 import Body from "./components/Body"
@@ -8,7 +8,7 @@ import Contact from "./components/Contact"
 import Error from "./components/Error"
 import RestaurantMenu from "./components/RestaurantMenu"
 
-
+const Grocery = lazy(()=>import("./components/Grocery"))
 const AppLayout = () => {
     return <div className="app">
         <Header />
@@ -35,6 +35,10 @@ const appRouter = createBrowserRouter([
             {
                 path:"/restaurants/:resId",
                 element:<RestaurantMenu/>
+            },
+            {
+                path:"/grocery",
+                element:<Suspense fallback={<h1>Loading</h1>}><Grocery/></Suspense>
             }
         ],
         errorElement:<Error/>
---
+++ src/components/Grocery.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Grocery() {
+  return (
+    <div>Grocery</div>
+  )
+}
+
+export default Grocery
+
